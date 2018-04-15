93d0d0cc4f04d225730c840a980db6ef72921e49
Pavel Vasiliev <pavel.v96@yandex.ru>
2018-04-15 12:19:31 +0300

    divide search layout into partials

--- app/src/main/java/com/pvasiliev/uplabs/MainActivity.kt
+++ app/src/main/java/com/pvasiliev/uplabs/MainActivity.kt
@@ -3,7 +3,7 @@ package com.pvasiliev.uplabs
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
-import kotlinx.android.synthetic.main.fragment_search.*
+import kotlinx.android.synthetic.main.view_recent.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
