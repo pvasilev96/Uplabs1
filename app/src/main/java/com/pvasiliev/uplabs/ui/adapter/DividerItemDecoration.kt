c84a4798c8638c108b857ed49172ba6d940fc22f
Pavel Vasiliev <pavel.v96@yandex.ru>
2018-04-21 13:43:23 +0300

    fix error with moshi

--- app/src/main/java/com/pvasiliev/uplabs/ui/adapter/DividerItemDecoration.kt
+++ app/src/main/java/com/pvasiliev/uplabs/ui/adapter/DividerItemDecoration.kt
@@ -6,22 +6,24 @@ import android.graphics.Paint
 import android.support.v7.widget.RecyclerView
 
 class DividerItemDecoration(private val left: Int, private val right: Int) : RecyclerView.ItemDecoration() {
+    private val strokeWidth = 2.0F
+
     private val paint = Paint().apply {
         color = Color.parseColor("#E0E0E0")
-        strokeWidth = 2.0F
+        strokeWidth = this@DividerItemDecoration.strokeWidth
     }
 
     override fun onDrawOver(canvas: Canvas, parent: RecyclerView, state: RecyclerView.State) {
-        val paddingLeft = parent.context.resources.getDimension(left)
-        val paddingRight = parent.context.resources.getDimension(right)
+        val paddingLeft = parent.context.resources.getDimensionPixelSize(left)
+        val paddingRight = parent.context.resources.getDimensionPixelSize(right)
         (0 until parent.childCount)
                 .map { parent.getChildAt(it) }
                 .forEach {
                     canvas.drawLine(
-                            paddingLeft,
-                            it.top.toFloat() + 2.0F,
+                            paddingLeft.toFloat(),
+                            it.top.toFloat() + strokeWidth,
                             it.width.toFloat() - paddingRight,
-                            it.top.toFloat() + 2.0F, paint
+                            it.top.toFloat() + strokeWidth, paint
                     )
                 }
     }
--- app/src/main/java/com/pvasiliev/uplabs/ui/posts/PostListPresenter.kt
+++ app/src/main/java/com/pvasiliev/uplabs/ui/posts/PostListPresenter.kt
@@ -1,5 +1,6 @@
 package com.pvasiliev.uplabs.ui.posts
 
+import com.arellomobile.mvp.InjectViewState
 import com.arellomobile.mvp.MvpPresenter
 import com.pvasiliev.uplabs.data.network.UplabsApi
 import io.reactivex.android.schedulers.AndroidSchedulers
@@ -7,6 +8,7 @@ import io.reactivex.schedulers.Schedulers
 import ru.terrakok.cicerone.Router
 import javax.inject.Inject
 
+@InjectViewState
 class PostListPresenter @Inject constructor(private val api: UplabsApi, private val router: Router) : MvpPresenter<PostListView>() {
     override fun onFirstViewAttach() {
         api.getLatest(0)
